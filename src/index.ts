b88f4ae42ac7c36df9af27c3245ac42a1c1b2498
gumingWu <15626185054@163.com>
2023-05-23 13:06:38 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,50 @@
 import { loadConfig, loadDotenv, watchConfig } from 'c12'
 
-export interface IWuConfig {
-  name?: string
-  age?: number
-  male?: boolean
-}
+(
+  async () => {
+    const config = await loadConfig({
+      name: 'wu',
+      globalRc: true,
+      packageJson: true,
+      dotenv: true,
+      defaults: {
+        name: 'defaults',
+        defaultKey: 'defaultValue',
+      },
+    })
 
-(async () => {
-  // const config = await loadConfig({
-  //   name: 'wu',
-  //   globalRc: true,
-  //   packageJson: true,
-  //   dotenv: true,
-  //   defaults: {
-  //     name: 'defaults',
-  //     defaultKey: 'defaultValue',
-  //   },
-  // })
+    console.log(config)
 
-  // console.log(config)
+    const env = await loadDotenv({
+      cwd: '.',
+      fileName: '.env',
+    })
 
-  // const env = await loadDotenv({
-  //   cwd: '.',
-  //   fileName: '.env',
-  // })
+    console.log(env)
+  }
+)()
 
-  // console.log(env)
+// (
+//   async () => {
+//     const config = await watchConfig({
+//       cwd: '.',
+//       name: 'wu',
+//       onWatch: (event) => {
+//         console.log('[watcher]', event.type, event.path)
+//       },
+//       acceptHMR({ oldConfig, newConfig, getDiff }) {
+//         const diff = getDiff()
+//         if (diff.length === 0) {
+//           console.log('No config changed detected!')
+//           return true // No changes!
+//         }
+//       },
+//       onUpdate({ oldConfig, newConfig, getDiff }) {
+//         const diff = getDiff()
+//         console.log(`Config updated:\n${diff.map(i => i.toJSON()).join('\n')}`)
+//       },
+//     })
 
-  const config = await watchConfig({
-    cwd: '.',
-    name: 'wu',
-    onWatch: (event) => {
-      console.log('[watcher]', event.type, event.path)
-    },
-    acceptHMR({ oldConfig, newConfig, getDiff }) {
-      const diff = getDiff()
-      if (diff.length === 0) {
-        console.log('No config changed detected!')
-        return true // No changes!
-      }
-    },
-    onUpdate({ oldConfig, newConfig, getDiff }) {
-      const diff = getDiff()
-      console.log(`Config updated:\n${diff.map(i => i.toJSON()).join('\n')}`)
-    },
-  })
-
-  console.log(config)
-})()
+//     console.log(config)
+//   },
+// )()
